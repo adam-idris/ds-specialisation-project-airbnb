c1ba09b9f5c7e1a2e1040ceb1fb8aef37dfa142a
adam-idris <adam.idris@live.co.uk>
2024-09-20 15:54:19 +0100

    Cleaning data

---
+++ tabular_data.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from ast import literal_eval
+
+df = pd.read_csv('listing.csv')
+
+def clean_tabular_data(df):
+
+    def remove_rows_with_missing_ratings(df):
+        ratings_columns = df.columns[df.columns.str.contains('rating')]
+        df = df.dropna(subset=ratings_columns)
+        
+        return df
+    
+    df = remove_rows_with_missing_ratings(df)
+
+    def combine_description_strings(df):
+        df = df.dropna(subset=['Description'])
+        
+        def clean_description(description):
+            
+            try:
+                description = literal_eval(description)
+                description = [item for item in description if item.strip()]
+                
+                if 'About this space' in description:
+                    description.remove('About this space')
+                    
+                description = ' '.join(description)
+                description = description.replace('\n', ' ')
+                
+                return description
+            
+            except (ValueError, SyntaxError):
+                
+                return description
+        
+        df['Description'] = df['Description'].apply(clean_description)
+        
+        return df
+    
+    df = combine_description_strings(df)
+
+    def set_default_feature_values(df):
+        columns = ['guests', 'beds', 'bathrooms', 'bedrooms']
+        df[columns] = df[columns].fillna(value=1)
+        
+        return df
+    
+    df = set_default_feature_values(df)
+    
+    return df
+
+df = clean_tabular_data(df)
+df.to_csv('cleaned_tabular_data.csv')
